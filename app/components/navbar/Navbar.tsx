dc7f6181604bb2ea7718a1967c8624f7ea486465
Dawitlior <dawitlior777@gmail.com>
2023-07-29 15:42:35 +0300

    navbar folder structure and design

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
+import UserMenu from './UserMenu'
 
 
 const Navbar = () => {
@@ -12,6 +13,7 @@ const Navbar = () => {
                     <div className='flex flex-grow items-center justify-between gap-3 md:gap-0'>
                         <Logo />
                         <Search />
+                        <UserMenu />
                     </div>
                 </Container>
             </div>
