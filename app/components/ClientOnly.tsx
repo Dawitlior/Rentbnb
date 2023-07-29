791acff7633d765abd34c8a71b94dbf1b053b745
Dawitlior <dawitlior777@gmail.com>
2023-07-29 19:19:01 +0300

    handle runtime error compo

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
+// for handling the unhandled runtime error/hydration error
+// check if im in server side rendering or not
+
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({
+    children
+}) => {
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true);
+    }, [])
+
+    if (!hasMounted) return null;
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default ClientOnly;
