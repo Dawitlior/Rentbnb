87ecfd665bed3426790f634fafacb70a07c5eb3a
Dawitlior <dawitlior777@gmail.com>
2023-07-29 19:48:33 +0300

    add

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -96,7 +96,11 @@ const Modal: React.FC<ModalProps> = ({
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
                 <div className=" flex flex-row items-center gap-4 w-full">
-                    <Button label="my button"  />
+                    <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                    />
                 </div>
               </div>
             </div>
