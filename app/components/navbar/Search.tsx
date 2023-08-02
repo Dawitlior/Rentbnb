165d4a10f7632502ecd2453a2e0084d184022c90
Dawitlior <dawitlior777@gmail.com>
2023-08-02 23:20:21 +0300

    caps lock

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -6,10 +6,10 @@ const Search = () => {
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-sm transition cursor-pointer">
           <div className="flex flex-row items-center justify-between">
             <div className="text-sm font-semibold px-6">
-              anything
+              Anything
             </div>
             <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-              any week
+              Any week
             </div>
             <div className="text-sm px-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
               <div className="hidden sm:block">Add guests</div>
