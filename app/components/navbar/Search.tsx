45dc2bd1672c8197cf4525842d0599f843abd291
Dawitlior <dawitlior777@gmail.com>
2023-08-03 00:25:55 +0300

    catrgories and navigation

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -6,13 +6,13 @@ const Search = () => {
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-sm transition cursor-pointer">
           <div className="flex flex-row items-center justify-between">
             <div className="text-sm font-semibold px-6">
-              Anything
+              AnyWhere
             </div>
             <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-              Any week
+              Any Week
             </div>
             <div className="text-sm px-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-              <div className="hidden sm:block">Add guests</div>
+              <div className="hidden sm:block">Add Guests</div>
               <div className="p-2 bg-rose-500 rounded-full text-white">
                 <BiSearch size={18}/>
               </div>
