7ab63aaae094434e612e63aa01ec8b92b199cace
Dawitlior <dawitlior777@gmail.com>
2023-08-04 20:57:49 +0300

    format document & rent modal changes

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,18 +2,55 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+import { useMemo, useState } from "react";
 
-const RentModal  = () => {
-    const rentModal = useRentModal()
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+};
+
+
+const RentModal = () => {
+  const rentModal = useRentModal();
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return 'Create';
+    }
+    return 'Next';
+  }, [step])
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined
+    }
+
+    return 'Back'
+  }, [step]);
+
+  
   return (
     <Modal
-    onSubmit={rentModal.onClose}
-    onClose={rentModal.onClose}
-    isOpen={rentModal.isOpen}
-    actionLabel="Submit"
-    title="Airbnb your home!"
+      onSubmit={rentModal.onClose}
+      onClose={rentModal.onClose}
+      isOpen={rentModal.isOpen}
+      actionLabel="Submit"
+      title="Airbnb your home!"
     />
   );
 };
 
-export default RentModal ;
+export default RentModal;
