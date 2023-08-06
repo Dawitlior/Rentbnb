0a6109ba4bd0847644fd7aff4fa8e3cceff3e410
Dawitlior <dawitlior777@gmail.com>
2023-08-06 22:48:27 +0300

    Cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "@/app/components/inputs/CountrySelect";
 import dynamic from "next/dynamic"
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -49,15 +50,17 @@ const RentModal = () => {
     }
   });
 
-  const category = watch('category');
   const location = watch('location');
+  const category = watch('category');
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
+  
   const Map = useMemo(() => dynamic(() => import ('../Map'),{
     ssr: false,
-  }),[location]);
+  }), [location]) // eslint-disable-next-line react-hooks/exhaustive-deps  
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -169,6 +172,21 @@ const RentModal = () => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+        title="Add a photo of your place"
+        subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload 
+        value={imageSrc}
+        onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     )
