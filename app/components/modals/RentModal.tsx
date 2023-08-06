efd12bf4796b5fa9de0056894245cc48312dcbeb
Dawitlior <dawitlior777@gmail.com>
2023-08-07 00:32:29 +0300

    add steps 4 and 5

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import CountrySelect from "@/app/components/inputs/CountrySelect";
 import dynamic from "next/dynamic"
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
 
 enum STEPS {
   CATEGORY = 0,
@@ -26,6 +27,7 @@ enum STEPS {
 const RentModal = () => {
   const rentModal = useRentModal();
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -187,6 +189,55 @@ const RentModal = () => {
         <ImageUpload 
         value={imageSrc}
         onChange={(value) => setCustomValue('imageSrc', value)}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+        title="How would you describe your place?"
+        subtitle="Short and sweet works best!"
+        />
+        <Input 
+        id="title"
+        label="Title"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+        title="Now, set your price"
+        subtitle="How much do you charge per night?"
+        />
+        <Input 
+        id="price"
+        label="Price"
+        formatPrice
+        type="number"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
         />
       </div>
     )
