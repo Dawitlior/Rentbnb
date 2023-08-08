3b75028d3152733a3fa33192af3d81d111f3fa33
Dawitlior <dawitlior777@gmail.com>
2023-08-08 11:28:39 +0300

    Favorites screen & Loading favorites with server component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -66,7 +66,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 />
                 <MenuItem
                   label="My favorites"
-                  onClick={() => { }}
+                  onClick={() => router.push("/favorites")}
                 />
                 <MenuItem
                   label="My reservations"
