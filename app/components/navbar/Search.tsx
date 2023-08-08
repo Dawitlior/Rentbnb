d232f932a87a5d03ba1ef59cc0c0d8a0ff945ded
Dawitlior <dawitlior777@gmail.com>
2023-08-08 13:49:13 +0300

    Filters modal Assigning various filters, add advanced querying logic to getListings

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,23 +1,28 @@
 "use client"
+import useSearchModal from "@/app/hooks/useSearchModal"
 import { BiSearch } from "react-icons/bi"
 
 const Search = () => {
+  const searchModal = useSearchModal();
+
   return (
-    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-sm transition cursor-pointer">
-          <div className="flex flex-row items-center justify-between">
-            <div className="text-sm font-semibold px-6">
-              AnyWhere
-            </div>
-            <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-              Any Week
-            </div>
-            <div className="text-sm px-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-              <div className="hidden sm:block">Add Guests</div>
-              <div className="p-2 bg-rose-500 rounded-full text-white">
-                <BiSearch size={18}/>
-              </div>
-            </div>
+    <div
+      onClick={searchModal.onOpen}
+      className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-sm transition cursor-pointer">
+      <div className="flex flex-row items-center justify-between">
+        <div className="text-sm font-semibold px-6">
+          AnyWhere
+        </div>
+        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+          Any Week
+        </div>
+        <div className="text-sm px-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+          <div className="hidden sm:block">Add Guests</div>
+          <div className="p-2 bg-rose-500 rounded-full text-white">
+            <BiSearch size={18} />
           </div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import LoginModal from './components/modals/LoginModal'
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -28,6 +29,7 @@ export default async function RootLayout({
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
