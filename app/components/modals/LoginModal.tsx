2d0e35e6739041043b01ef6bbb1d1732ee995a32
Dawitlior <dawitlior777@gmail.com>
2023-08-08 16:41:39 +0300

    move input and change structure

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -12,7 +12,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 
 import Modal from "./Modal";
-import Input from "../inputs/Input";
+import Input from "../Input";
 import Heading from "../Heading";
 import Button from "../Button";
 
@@ -102,7 +102,7 @@ const LoginModal = () => {
       />
       <div className="text-neutral-500 text-center mt-4 font-light gap-2">
         <p>
-          First time using Airbnb? 
+          First time using Airbnb?
           <span
             onClick={toggle}
             className="text-neutral-800 cursor-pointer hover:underline"
