a315ac56d7c2c094c4b7042033bbd52686d1c732
Dawitlior <dawitlior777@gmail.com>
2023-08-08 12:49:11 +0300

    add

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -74,7 +74,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 />
                 <MenuItem
                   label="My properties"
-                  onClick={() => { }}
+                  onClick={() => router.push("/properties")}
                 />
                 <MenuItem
                   label="Airbnb your home"
