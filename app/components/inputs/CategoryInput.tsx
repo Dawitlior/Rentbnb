6358c784c78350583c5d4f0e98582b3ade9de793
Dawitlior <dawitlior777@gmail.com>
2023-08-29 05:14:11 +0300

    F-D

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -17,9 +17,8 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
   return (
     <div
       onClick={() => onClick(label)}
-      className={` rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${
-        selected ? "border-black" : "border-neutral-200"
-      }`}
+      className={` rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${selected ? "border-black" : "border-neutral-200"
+        }`}
     >
       <Icon size={30} />
       <div className="font-semibold">{label}</div>
